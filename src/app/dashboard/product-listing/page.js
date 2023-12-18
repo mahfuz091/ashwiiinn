d1dd27f48ba55be9c2862df4f3b79f50bfbf2578
mahfuz091 <engmahfuz14@gmail.com>
2023-12-18 20:22:12 +0600

    header added

---
+++ src/app/dashboard/product-listing/page.js
@@ -0,0 +1,13 @@
+import Header from '@/Components/Header/Header';
+import React from 'react';
+
+const ProductListingPage = () => {
+    return (
+        <div>
+            <Header title={"Product Listings"}></Header>
+
+        </div >
+    );
+};
+
+export default ProductListingPage;
