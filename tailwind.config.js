652eea272a04e7da1230ed49a8885caa3f71bb1b
mahfuz091 <engmahfuz14@gmail.com>
2023-12-20 20:29:29 +0600

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  // purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
 
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
