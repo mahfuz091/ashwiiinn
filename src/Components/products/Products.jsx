3d0a9134304644dbfa37cfe7b566e3d12fd6a2cf
mahfuz091 <engmahfuz14@gmail.com>
2023-12-28 18:46:11 +0600

    product fetch and show

--- src/Components/products/Products.jsx
+++ src/Components/products/Products.jsx
@@ -1,14 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import SingleProduct from "./SingleProduct";
 
 const Products = () => {
+  const [isOpen, SetIsOpen] = useState(true)
+
+  const [products, SetProducts] = useState([])
   useEffect(() => {
     fetch("/moondust.json")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => SetProducts(data));
+
   }, []);
 
-  return <div></div>;
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.style.height = isOpen ? '0' : `${contentRef.current.scrollHeight}px`;
+    }
+  }, [isOpen]);
+  return <div className="container mx-auto mb-[94px]">
+    <div className='Distillate-Cartridges px-[33px] py-[36px] bg-[#F5F5F5] rounded-[15px]'>
+      <div className='flex justify-between items-center'>
+        <h6 className="text-[#000] text-[20px] font-medium">Moon Dust - 1G Distillate Cartridges</h6>
+        <div className='flex justify-between items-center gap-[12px]'>
+          <p className="text-[#535353] text-[15px] font-normal">Showing 1 - {products.length} of {products.length} results</p>
+          <button onClick={() => SetIsOpen(!isOpen)}>
+            <img className={isOpen ? "" : "rotate-180 transition-all"} src='/DownArrowBlack.png' alt='' />
+          </button>
+        </div>
+      </div>
+      <div className={`collapse-content ${isOpen ? 'collapsed' : ''}`}>
+        {
+          products?.map((product, index) => <SingleProduct key={index} product={product}></SingleProduct>)
+        }
+      </div>
+    </div>
+  </div>;
 };
 
 export default Products;
