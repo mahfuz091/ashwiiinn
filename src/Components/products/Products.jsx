ddc7f3f5e51bf936f1c97e3da9d4bb71d21e78ae
mahfuz091 <engmahfuz14@gmail.com>
2023-12-30 01:19:36 +0600

    up-2

--- src/Components/products/Products.jsx
+++ src/Components/products/Products.jsx
@@ -4,18 +4,18 @@ import SingleProductCard from "./SingleProductCard";
 // import LoadProductData from "@/utils/LoadProductData/LoadProductData";
 
 const Products = () => {
-  const [products, SetProducts] = useState([]);
+  const [productsData, SetProductsData] = useState([]);
 
   useEffect(() => {
     fetch("/products.json")
       .then((res) => res.json())
-      .then((data) => SetProducts(data));
+      .then((data) => SetProductsData(data));
   }, []);
   // const products = LoadProductData();
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-[31px] gap-y-[48px]'>
-      {products.map((product, index) => (
+      {productsData.map((product, index) => (
         <SingleProductCard key={index} product={product}></SingleProductCard>
       ))}
     </div>
