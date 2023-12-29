c9d5935277cb1de62922e70451aab6ee1094e80b
mahfuz091 <engmahfuz14@gmail.com>
2023-12-29 20:36:15 +0600

    added details view banner

--- src/app/shop-products/[id]/page.js
+++ src/app/shop-products/[id]/page.js
@@ -14,26 +14,27 @@ const SingleProductPage = ({ params }) => {
                         <div className='flex items-center'>
                             <div className='max-w-[600px] w-full'>
                                 <h4 className='text-[#000] text-[50px] font-bold text-center'>Goodfellas Disposable Watermelon OG -1G</h4>
-                                <p>Our organic Dragon fruit powder is perfect for making smothies, smoothie bowls, agua fresca’s chia pudding and much more.</p>
+                                <p className='text-[#000] text-[24px] font-normal text-center'>Our organic Dragon fruit powder is perfect for making smothies, smoothie bowls, agua fresca’s chia pudding and much more.</p>
                             </div>
                             <img src="/product-detail.png" alt="" />
                         </div>
                         <div className='rounded-[20px] bg-[#fff] shadow-secoundary px-[27px] py-[42px]'>
-                            <p>Freeze Dried Powder</p>
-                            <select name="" id="">
+                            <p className='text-[#000] text-[15px] font-medium text-center mb-[15px]'>Freeze Dried Powder</p>
+                            <select className='details-select py-[25px] pl-[26px] pr-[142px] bg-[#FFB800] rounded-[40px] text-[#000] text-[20px] font-semibold focus:border-none mb-[25px]' name="" id="">
                                 <option value="">Organic Dragon Fruit</option>
                             </select>
-                            <p>$28.99 per bag</p>
-                            <p>8 oz powder per bag</p>
+                            <p className="text-[#000] text-[20px] font-medium text-center">$28.99 per bag</p>
+                            <p className="text-[#535353] text-[15px] font-medium text-center mb-[50px]">8 oz powder per bag</p>
                             <form action="">
                                 <div className="form-control">
-                                    <label htmlFor="">Quantity</label>
-                                    <input type="text" name="" id="" placeholder="Amount in Cases" />
+                                    <label className="text-[#000] text-[17px] font-medium " htmlFor="">Quantity</label>
+                                    <input className="w-full py-[13px] pl-[15px] bg-[#F1F1F1] border-[1px] border-[#D6D6D6] rounded-[5px] mt-[15px]" type="text" name="" id="" placeholder="Amount in Cases" />
 
+                                    <p className='text-right text-[#535353] text-[13px] font-normal pt-[12px] mb-[44px]'>Min : 1, Max -</p>
                                 </div>
-                                <button type="submit">Add To Cart</button>
+                                <button className='py-[17px] w-full flex justify-center rounded-[40px] bg-[#6BCB77] text-[#fff] text-[17px] font-semibold mb-[20px]' type="submit">Add To Cart</button>
                             </form>
-                            <button>Request Sample</button>
+                            <button className='py-[17px] w-full flex justify-center rounded-[40px] border-[#6BCB77] border-[1px] text-[#6BCB77] text-[17px] font-semibold mb-[20px]'>Request Sample</button>
                         </div>
                     </div>
                 </div>
